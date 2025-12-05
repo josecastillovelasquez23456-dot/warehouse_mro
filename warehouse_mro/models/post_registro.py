4bffc94a3901f2c65c751ce3884001346af05857
josecastillovelasquez23456-dot <josecastillovelasquez23456@gmail.com>
2025-12-05 13:13:05 -0500

    Update post_registro.py

--- warehouse_mro/models/post_registro.py
+++ warehouse_mro/models/post_registro.py
@@ -5,13 +5,11 @@ class PostRegistro(db.Model):
     __tablename__ = "post_registro"
 
     id = db.Column(db.Integer, primary_key=True)
-
     bulto_id = db.Column(db.Integer, db.ForeignKey("bultos.id"))
     codigo_material = db.Column(db.String(120))
     cantidad_sistema = db.Column(db.Integer)
     cantidad_real = db.Column(db.Integer)
     diferencia = db.Column(db.Integer)
-
     registrado_por = db.Column(db.String(100))
     fecha_registro = db.Column(db.DateTime, default=datetime.utcnow)
 
